bba15530ce85d92f59fb1dea19aca3763f51721d
Chloe <clo.gendreau@gmail.com>
2023-01-09 10:02:32 +0100

    exercice 3

---
+++ 03_elementModification.js
@@ -0,0 +1,8 @@
+// Créez un script qui lors de son execution, modifie le texte contenu dans le premier “button” trouvé
+// dans le dom en “hello world”
+// ⚠ ce script est a exécution directe, il ne nécessite pas de module.export ni de déclaration de fonction
+
+ document.getElementsByTagName("button")[0].innerHTML = "hello world";
+
+/* possibilité de l'écrire aussi avec querySelector() =>
+document. querySelector("button").innerHTML = "hello world" */
