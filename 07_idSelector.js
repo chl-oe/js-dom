c79b7b4f105cc55ba1a0e84cb9733c1a717ce8b7
Chloe <clo.gendreau@gmail.com>
2023-01-09 15:56:31 +0100

    exercice 07

---
+++ 07_idSelector.js
@@ -0,0 +1,11 @@
+/* Dans cet exercice, vous devrez sélectionner un champ input grâce a son id et me renvoyer
+son contenu lors de l’execution de votre fonction.
+⚠ un module.export est attendu */
+
+function idSelector(){
+  return document.getElementById("input");
+}
+
+console.log(idSelector());
+
+module.exports = idSelector;
