5cf6070d21fd09a885744cd959c6f8b3b89ec484
Chloe <clo.gendreau@gmail.com>
2023-01-09 10:20:13 +0100

    update ex.3 + add excercice 4

--- 03_elementModification.js
+++ 03_elementModification.js
@@ -2,7 +2,7 @@
 // dans le dom en “hello world”
 // ⚠ ce script est a exécution directe, il ne nécessite pas de module.export ni de déclaration de fonction
 
- document.getElementsByTagName("button")[0].innerHTML = "hello world";
+document.getElementsByTagName("button")[0].innerHTML = "hello world";
 
 /* possibilité de l'écrire aussi avec querySelector() =>
-document. querySelector("button").innerHTML = "hello world" */
+document.querySelector("button").innerHTML = "hello world" */
---
+++ 04_elementStyle.js
@@ -0,0 +1,5 @@
+/* Dans cet exercice, vous devrez sélectionner le premier bouton dans le dom et changer sa couleur de fond en rouge, en
+utilisant uniquement le javascript (.css et <style>interdis</style>)
+⚠ ce script est a exécution directe, il ne nécessite pas de module.export ni de déclaration de fonction */
+
+document.getElementsByTagName("button")[0].style.backgroundColor = "red";
