19c9a88712dd3c33a304c234ebc83ff33f57b6e3
Chloe <clo.gendreau@gmail.com>
2023-01-10 17:32:04 +0100

    exercice 11

---
+++ 11_domInjection.js
@@ -0,0 +1,8 @@
+/* créez un script qui injectera un element <p></p>, possédant comme contenu “hello world” comme premier enfant de
+l’élément préexistant dans le dom div#content
+⚠ ce script est a exécution directe, il ne nécessite pas de module.export ni de déclaration de fonction */
+
+let container = document.getElementById('div#content');
+let injection = document.createElement("p");
+injection.innerHTML = "hello world";
+container.appendChild(injection);
