87df2864132bd8f005e6b78f87f00d8fb77bb62a
Chloe <clo.gendreau@gmail.com>
2023-01-10 11:19:17 +0100

    update ex.08

--- 08_querySelector.js
+++ 08_querySelector.js
@@ -15,6 +15,10 @@ element.map(e => e.style.backgroundColor = "blue"); */
 
 document.querySelector(".btn").style.backgroundColor = "blue";
 document.querySelector(".btn.danger").style.backgroundColor = "red";
-document.querySelector(".btn.warning").style.backgroundColor = "yellow";
 document.querySelector(".btn.secondary").style.backgroundColor = "grey";
+document.querySelector(".btn.warning").style.backgroundColor = "yellow";
+document.querySelector(".btn.secondary.danger").style.backgroundColor = "red";
+document.querySelector(".btn.secondary.warning").style.backgroundColor = "yellow";
+document.querySelector(".btn.danger.warning").style.backgroundColor = "yellow";
+
 
