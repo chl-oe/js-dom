5bf6e08b9e3d9ae5b2b7d311198244f46290f307
Chloe <clo.gendreau@gmail.com>
2023-01-11 10:33:26 +0100

    exercice 12

---
+++ 12_domDeletion.js
@@ -0,0 +1,9 @@
+/* Supprimer du dom tous les éléments <p></p>
+⚠ ce script est a exécution directe, il ne nécessite pas de module.export ni de déclaration de fonction */
+
+let element = document.querySelectorAll("p");
+element = [...element];
+element.map(e => e.remove());
+console.log(element);
+
+
