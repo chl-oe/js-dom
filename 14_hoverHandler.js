e0f792386eeea37da3501c7039faf9ec54e722ed
Chloe <clo.gendreau@gmail.com>
2023-01-11 16:05:09 +0100

    exercice 14

---
+++ 14_hoverHandler.js
@@ -0,0 +1,13 @@
+/* Ajouter l’élément <p id=”result”>hovered</p> comme enfant de div.content lorsque l’élément <p
+id=”hoverme”>hoverme</p>
+est survolé par la souris */
+
+let myElement = document.getElementById("hoverme");
+myElement.addEventListener("mouseover", function() {
+let container = document.querySelector(".content");
+let injection = document.createElement("p")
+injection.setAttribute("id", "result")
+injection.textContent = "hovered";
+container.appendChild(injection);
+console.log(injection);
+});
