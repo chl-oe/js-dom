b574dd93763697821fb0beba2681662191f5c0f0
Chloe <clo.gendreau@gmail.com>
2023-01-11 11:44:47 +0100

    exercice 13

---
+++ 13_clickHandler.js
@@ -0,0 +1,11 @@
+/* Ajouter l’élément <p id=”result”>clicked</p> comme enfant de div.content
+lorsque l’élément button#clickme est cliqué */
+
+let myButton = document.getElementById("clickme");
+myButton.addEventListener("click", function() {
+let container = document.querySelector(".content");
+let injection = document.createElement("p")
+injection.setAttribute("id", "result")
+injection.textContent = "clicked";
+container.appendChild(injection);
+});
